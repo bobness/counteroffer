572620ca1b1bc30437d61d9b5381ef012500e3ca
Bob Stark <bob@datagotchi.net>
2021-07-22 20:44:20 -0700

    Added package-lock.json

--- co.app.js
+++ co.app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(async (req, res, next) => {
+  // TODO: store secrets somewhere secure
   const client = new Client({
     user: 'postgres',
     password: 'p4ssw0rd',
